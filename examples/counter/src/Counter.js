19341672cc9a927f0c0fe2b14e5e13ba47f4a3a4
Quan Do <quan.do@ugroop.com.au>
2017-08-06 19:43:43 +1000

    Add Counter testings

--- examples/counter/src/Counter.js
+++ examples/counter/src/Counter.js
@@ -1,24 +1,23 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-const increase = (value) => value + 1;
-const decrease = (value) => value - 1;
-
-const increaseIfOdd = (value) => value % 2 ? value + 1 : value;
-const decreaseIfEven = (value) => value % 2 === 0 ? value - 1 : value;
+export const increase = (value) => value + 1;
+export const decrease = (value) => value - 1;
+export const increaseIfOdd = (value) => value % 2 ? value + 1 : value;
+export const decreaseIfEven = (value) => value % 2 === 0 ? value - 1 : value;
 
 const Counter = ({ resaga }) =>
   <p>
     Value {resaga.getValue('counter')}
     <br /><br />
-    <button onClick={() => resaga.setValue('counter', 0)}>Init value</button>
-    <button onClick={() => resaga.setValue('counter', 100)}>Set Counter to 100</button>
+    <button id="init" onClick={() => resaga.setValue('counter', 0)}>Init value</button>
+    <button id="set100" onClick={() => resaga.setValue('counter', 100)}>Set Counter to 100</button>
     <br /><br />
-    <button onClick={() => resaga.setValue('counter', increase)}>Increase</button>
-    <button onClick={() => resaga.setValue('counter', decrease)}>Decrease</button>
+    <button id="increase" onClick={() => resaga.setValue('counter', increase)}>Increase</button>
+    <button id="decrease" onClick={() => resaga.setValue('counter', decrease)}>Decrease</button>
     <br /><br />
-    <button onClick={() => resaga.setValue('counter', increaseIfOdd)}>Increase if odd</button>
-    <button onClick={() => resaga.setValue('counter', decreaseIfEven)}>Decrease if even</button>
+    <button id="increaseIfOdd" onClick={() => resaga.setValue('counter', increaseIfOdd)}>Increase if odd</button>
+    <button id="decreaseIfEven" onClick={() => resaga.setValue('counter', decreaseIfEven)}>Decrease if even</button>
   </p>;
 
 Counter.propTypes = {
---
+++ examples/counter/src/__tests__/Counter.test.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Counter, { increase, increaseIfOdd, decrease, decreaseIfEven } from '../Counter';
+
+describe('Counter', () => {
+  const key = 'counter';
+  describe('<Counter />', () => {
+    const resaga = {
+      setValue: jest.fn(),
+      getValue: jest.fn(),
+    };
+    const rendered = shallow(<Counter resaga={resaga} />);
+
+    afterEach(() => {
+      resaga.setValue.mockClear();
+      resaga.getValue.mockClear();
+    });
+
+    it('should render without exploding', () => {
+      expect(rendered).toBeDefined();
+    });
+    it('should have resaga prop', () => {
+      const component = rendered.instance();
+      const props = component.props;
+      expect(props).toBeDefined();
+      expect(props.resaga).toBe(resaga);
+    });
+    it('#init', () => {
+      const btn = rendered.find('#init');
+      expect(btn.length).toBe(1);
+      btn.simulate('click');
+      expect(resaga.setValue).toBeCalledWith(key, 0);
+    });
+    it('#set100', () => {
+      const btn = rendered.find('#set100');
+      expect(btn.length).toBe(1);
+      btn.simulate('click');
+      expect(resaga.setValue).toBeCalledWith(key, 100);
+    });
+    it('#increase', () => {
+      const btn = rendered.find('#increase');
+      expect(btn.length).toBe(1);
+      btn.simulate('click');
+      expect(resaga.setValue).toBeCalledWith(key, increase);
+    });
+    it('#decrease', () => {
+      const btn = rendered.find('#decrease');
+      expect(btn.length).toBe(1);
+      btn.simulate('click');
+      expect(resaga.setValue).toBeCalledWith(key, decrease);
+    });
+    it('#increaseIfOdd', () => {
+      const btn = rendered.find('#increaseIfOdd');
+      expect(btn.length).toBe(1);
+      btn.simulate('click');
+      expect(resaga.setValue).toBeCalledWith(key, increaseIfOdd);
+    });
+    it('#decreaseIfEven', () => {
+      const btn = rendered.find('#decreaseIfEven');
+      expect(btn.length).toBe(1);
+      btn.simulate('click');
+      expect(resaga.setValue).toBeCalledWith(key, decreaseIfEven);
+    });
+  });
+
+  describe('increase', () => {
+    it('should work correctly', () => {
+      expect(increase(1)).toBe(2);
+    });
+  });
+
+  describe('increaseIfOdd', () => {
+    it('should work correctly - odd', () => {
+      expect(increaseIfOdd(1)).toBe(2);
+    });
+    it('should work correctly - even', () => {
+      expect(increaseIfOdd(2)).toBe(2);
+    });
+  });
+
+  describe('decrease', () => {
+    it('should work correctly', () => {
+      expect(decrease(1)).toBe(0);
+    });
+  });
+
+  describe('decreaseIfEven', () => {
+    it('should work correctly - odd', () => {
+      expect(decreaseIfEven(1)).toBe(1);
+    });
+    it('should work correctly - even', () => {
+      expect(decreaseIfEven(2)).toBe(1);
+    });
+  });
+});
