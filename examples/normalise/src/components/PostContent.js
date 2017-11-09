3b6673d86b3d13c4712abc7130f0aae477be425e
Quan Do <quan.do@ugroop.com.au>
2017-11-09 12:15:35 +1100

    Flatten redux value objects + Implement setValue remotely

--- examples/normalise/src/components/PostContent.js
+++ examples/normalise/src/components/PostContent.js
@@ -4,15 +4,32 @@ import resaga from '../../../../build';
 import { PAGE as OTHER_PAGE } from '../containers/config';
 
 export class PostContent extends React.PureComponent {
+  setVisible = (visibility) => () =>
+    this.props.resaga.setValue({ visibility });
+
+  increaseVisible = () =>
+    this.props.resaga.setValue({ visibility: (value) => value + 1 }, console.log);
+
+  decreaseVisible = () =>
+    this.props.resaga.setValue({ visibility: (value) => value - 1 }, console.log);
+
   render = () => {
     const { title } = this.props;
     console.log('PostContent render', `${title.slice(0, 10)}...`);
 
-    return <b>{title}</b>;
+    return (
+      <div>
+        <b>{title}</b> <br />
+        <button onClick={this.setVisible(2)}>Set visible 2</button> <br />
+        <button onClick={this.decreaseVisible}>decreaseVisible</button>
+        <button onClick={this.increaseVisible}>increaseVisible</button>
+      </div>
+    );
   };
 }
 
 PostContent.propTypes = {
+  resaga: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
 };
 
@@ -20,6 +37,9 @@ PostContent.defaultProps = {
 };
 
 export default resaga({
+  setValue: {
+    visibility: [OTHER_PAGE, 'visible'],
+  },
   value: {
     title: {
       keyPath: [OTHER_PAGE, 'posts'],
